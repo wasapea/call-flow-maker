2a21e58475dfab88fc9b9ed6c16759936b1c7511
Skylar Baker <skylar.baker@metronetinc.com>
2021-12-22 14:48:18 -0800

    Formatting changes for extension

--- CallFlowMakerTests.py
+++ CallFlowMakerTests.py
@@ -2,3 +2,13 @@
 # Author: Skylar Baker
 # Title: Tests for text based call flow maker
 ##########
+
+from CallFlowMaker import *
+import unittest
+from unittest.mock import patch, Mock
+
+# This is just to populate the tests file so VSCode extensions pick the tests up properly
+# Note to self: start all test functions with 'test' so extensions work properly
+class BlankTest(unittest.TestCase):
+    def test_nothing(self):
+        pass  
