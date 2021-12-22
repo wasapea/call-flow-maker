8907de11f1dfed726c3b93a1c9d0522530c297a0
Skylar Baker <skylar.baker@metronetinc.com>
2021-12-22 15:54:17 -0800

    Added helper function and test to get yes/no input

--- CallFlowMaker.py
+++ CallFlowMaker.py
@@ -2,3 +2,35 @@
 # Author: Skylar Baker
 # Title: Text based call flow maker
 ##########
+
+# Notes
+# Questions passed to helpers should NOT include a newline, helpers add the newline
+
+# Imports
+import re
+
+# Helpers
+def get_yes_no_input(question, default=2):
+    """
+        Get an input from the end user and verify it is a y or n
+
+        Inputs:
+            question - string to ask the user
+            default (1) - 1 defaults to yes, 0 defaults to no, 2 has no default
+    """
+    answer_format = re.compile(r"^Y|y|N|n$")
+    answer = input("{question}\ny/n\n".format(question=question))
+
+    while not re.match(answer_format, answer):
+        if not answer:
+            if default == 0:
+                return False
+            elif default == 1:
+                return True
+        print("Please enter y or n")
+        answer = input("{question}\n".format(question=question))
+
+    if re.match(re.compile(r"y|Y"), answer):
+        return True
+    elif re.match(re.compile(r"n|N"), answer):
+        return False
--- CallFlowMakerTests.py
+++ CallFlowMakerTests.py
@@ -9,6 +9,23 @@ from unittest.mock import patch, Mock
 
 # This is just to populate the tests file so VSCode extensions pick the tests up properly
 # Note to self: start all test functions with 'test' so extensions work properly
-class BlankTest(unittest.TestCase):
-    def test_nothing(self):
-        pass  
+#class BlankTest(unittest.TestCase):
+#    def test_nothing(self):
+#        pass  
+
+class TestHelpers(unittest.TestCase):
+    @patch('builtins.input', side_effect=["a", "y", "", "Y"])
+    def test_yes_input(self, inputs):
+        self.assertTrue(get_yes_no_input("test q"))
+        self.assertTrue(get_yes_no_input("test q", 1))
+        self.assertTrue(get_yes_no_input("test q", 0))
+
+    @patch('builtins.input', side_effect=["a", "n", "N", ""])
+    def test_no_input(self, inputs):
+        self.assertFalse(get_yes_no_input("test q"))
+        self.assertFalse(get_yes_no_input("test q", 1))
+        self.assertFalse(get_yes_no_input("test q", 0))
+
+
+
+unittest.main()
